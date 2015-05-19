b344e77777c4b642a63fe8f785c78c47b6cad000
Jesse Emond <emond.jesse@gmail.com>
2015-05-19 14:32:27 -0400

    + predict values with 31 outputs

--- predict.cpp
+++ predict.cpp
@@ -2,8 +2,13 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <utility>
+#include <stdexcept>
 
 using namespace std;
+
+// pair of possible outcomes
+pair<int, int> predict_next_rand(const vector<int>& values);
 
 int main() {
   vector<int> values;
@@ -13,14 +18,27 @@ int main() {
     cout << i << ": " << value << endl;
   }
 
-  cout << "o0: " << values[values.size()-31] << endl;
-  cout << "o28: " << values[values.size()- 3] << endl;
-  int prediction1 = (values[values.size() - 31] + values[values.size() - 3]);
-  int prediction2 = (values[values.size() - 31] + values[values.size() - 3] + 1);
-  cout << "next one will be either: " << endl;
-  cout << "1) " << prediction1 << endl;
-  cout << "2) " << prediction2 << endl;
 
-  cout << "actual: " << rand() << endl;
+  for (int i = 0; i < 100; ++i) {
+    const auto predictions = predict_next_rand(values);
+    int actual = rand();
+    cout << "actual: " << actual << " ";
+    cout << "got it? " << (predictions.first == actual || predictions.second == actual) << endl;
+    values.push_back(actual);
+  }
 }
 
+
+pair<int, int> predict_next_rand(const vector<int>& values) {
+  // http://www.mathstat.dal.ca/~selinger/random/
+
+  if (values.size() < 31) throw invalid_argument("can only predict with >= 31 outputs");
+
+  int o0 = values[values.size() - 31];
+  int o28 = values[values.size() - 3];
+
+  int prediction1 = (o0 + o28) % (1u << 31);
+  int prediction2 = (o0 + o28 + 1) % (1u << 31);
+
+  return make_pair(prediction1, prediction2);
+}
