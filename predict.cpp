5b24eb75b5bff8e642b4d7d1715fce634add6bb1
Jesse Emond <emond.jesse@gmail.com>
2015-05-19 15:19:01 -0400

    + stats about which one is right

--- predict.cpp
+++ predict.cpp
@@ -19,13 +19,24 @@ int main() {
   }
 
 
-  for (int i = 0; i < 100; ++i) {
+  const int total = 10000;
+  int first = 0;
+  int second = 0;
+  for (int i = 0; i < total; ++i) {
     const auto predictions = predict_next_rand(values);
     int actual = rand();
     cout << "actual: " << actual << " ";
-    cout << "got it? " << (predictions.first == actual || predictions.second == actual) << endl;
+    cout << "got it? " << (predictions.first == actual || predictions.second == actual);
+    const bool firstWasRight = predictions.first == actual;
+    cout << " 1or2: " << (firstWasRight ? 1 : 2) << endl;
+    if (firstWasRight) ++first;
+    else ++second;
     values.push_back(actual);
   }
+
+  cout << "first right : " << first  << endl;
+  cout << "second right: " << second << endl;
+  if (total != first + second) cout << "OOPS" << endl;
 }
 
 
@@ -37,8 +48,8 @@ pair<int, int> predict_next_rand(const vector<int>& values) {
   int o0 = values[values.size() - 31];
   int o28 = values[values.size() - 3];
 
-  int prediction1 = (o0 + o28) % (1u << 31);
-  int prediction2 = (o0 + o28 + 1) % (1u << 31);
+  int prediction1 = (o0 + o28) % (1u << 31); // ~75% more likely (http://stackoverflow.com/a/14679656/395386)
+  int prediction2 = (o0 + o28) % (1u << 31) + 1;
 
   return make_pair(prediction1, prediction2);
 }
