efba88871b40f7b57f2051524f84fb99b1537371
Jesse Emond <emond.jesse@gmail.com>
2015-06-04 09:09:51 -0400

    * refactor test output

--- predict.cpp
+++ predict.cpp
@@ -4,36 +4,37 @@
 #include <vector>
 #include <utility>
 #include <stdexcept>
+#include <string>
 
 using namespace std;
 
-// pair of possible outcomes
-typedef pair<int, int> predictions_t;
-predictions_t predict_glibc_rand(const vector<int>& values);
+int predict_glibc_rand(const vector<int>& outputs);
 
 template <class Predictor>
-void test_predictor(Predictor predictor);
+void test_predictor(const string& testName, Predictor predictor);
 
 int main() {
-  test_predictor(predict_glibc_rand);
+  test_predictor("glibc's TYPE_1 random (default)", predict_glibc_rand);
 
   return 0;
 }
 
 
 /* Additive feedback generator predictor
+ * Predicts with ~75% success
  * Specifically for glibc's TYPE_1 rand (which is the default because by default srand gives a state
  * of 128 bytes to initstate, which gives TYPE_1 random, according to: http://stackoverflow.com/a/25819262/395386)
  */
-predictions_t predict_glibc_rand(const vector<int>& values) {
+int predict_glibc_rand(const vector<int>& outputs) {
   // info coming from:
   // http://www.mathstat.dal.ca/~selinger/random/
+
   // for simplicity, only focus on predicting the output after 31 outputs have already been
   // generated (equation #4)
-  if (values.size() < 31) throw invalid_argument("can only predict with >= 31 outputs");
+  if (outputs.size() < 31) throw invalid_argument("only predicting with >= 31 outputs");
 
-  int o0 = values[values.size() - 31];
-  int o28 = values[values.size() - 3];
+  int o31 = outputs[outputs.size() - 31];
+  int o3 = outputs[outputs.size() - 3];
 
   // random() will return (state[i-31] + state[i-3])>>1,
   // so we don't get to see the least significant bits (LSB) of the states when
@@ -49,50 +50,39 @@ predictions_t predict_glibc_rand(const vector<int>& values) {
   // 1 + 1 = (1)1 <- 1/4 probability with uniform distribution
   // Therefore, we can expect the LSBs from states to only affect the resulting
   // LSB of the output 25% of the time.
-  int prediction1 = (o0 + o28) % (1u << 31); // ~75% more likely (http://stackoverflow.com/a/14679656/395386)
-  int prediction2 = (o0 + o28) % (1u << 31) + 1;
+  int prediction1 = (o31 + o3) % (1u << 31); // ~75% more likely (http://stackoverflow.com/a/14679656/395386)
+  //int prediction2 = (o31 + o3) % (1u << 31) + 1; // 25% chance
 
-  return make_pair(prediction1, prediction2);
+  return prediction1; // pick the most likely
 }
 
 template <class Predictor>
-void test_predictor(Predictor predictor) {
+void test_predictor(const string& testName, Predictor predictor) {
+  cout << "Predicting output for " << testName << ":" << endl;
+
   const int INITIAL = 31;
-  vector<int> values;
+  vector<int> outputs;
   for (int i = 0; i < INITIAL; ++i) {
-    int value = rand();
-    values.push_back(value);
-    cout << i << ": " << value << endl;
+    int output = rand();
+    outputs.push_back(output);
   }
 
 
   const int TOTAL_TESTS = 10000;
-  int first = 0;
-  int second = 0;
+  int correct_guesses = 0;
   for (int i = 0; i < TOTAL_TESTS; ++i) {
-    const auto predictions = predict_glibc_rand(values);
+    const auto prediction = predict_glibc_rand(outputs);
 
     const int actual = rand();
-    cout << "actual: " << actual << " ";
+    outputs.push_back(actual);
 
-    const bool wasRight = predictions.first == actual ||
-      predictions.second == actual;
-    cout << "got it? " << wasRight;
+    const bool was_right = prediction == actual;
 
-    if (wasRight) {
-      const bool firstWasRight = predictions.first == actual;
-      cout << " first or second guess: " << (firstWasRight ? 1 : 2);
-
-      if (firstWasRight) ++first;
-      else ++second;
-    }
-    cout << endl;
-
-    values.push_back(actual);
+    correct_guesses += was_right;
   }
 
-  cout << "first right : " << first  << endl;
-  cout << "second right: " << second << endl;
-  if (TOTAL_TESTS != first + second)
-    cout << "!! Did not always guess right." << endl;
+  cout << "Success rate: " << correct_guesses << " / " << TOTAL_TESTS;
+  cout << " (" << (static_cast<float>(correct_guesses)/TOTAL_TESTS) << ")" << endl;
+
+  cout << endl;
 }
